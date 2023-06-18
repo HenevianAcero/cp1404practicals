753d70c08e07a383d5e0cd248eea8dcd74d2d77b
HenevianAcero <henevian.acero@my.jcu.edu.au>
2023-06-18 12:13:56 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,21 @@
 
-length_of_password = 8
-password = input("Please enter a password: ")
-while len(password) <= length_of_password:
-    print("Invalid password!")
+def main():
+    '''Returns password as stars while checking that it has more than the minimum number of characters'''
+    length_of_password = 8
+    password = get_password(length_of_password)
+    password_stars(password)
+
+
+def password_stars(password):
+    print(len(password) * "*")
+
+
+def get_password(length_of_password):
     password = input("Please enter a password: ")
-print(len(password) * "*")
+    while len(password) <= length_of_password:
+        print("Invalid password!")
+        password = input("Please enter a password: ")
+    return password
+
+
+main()
