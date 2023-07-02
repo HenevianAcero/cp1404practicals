68b5dc8f9c9ea0dd860638e6655f183df3eb74bb
HenevianAcero <henevian.acero@my.jcu.edu.au>
2023-07-02 19:30:19 +1000

    Hexadecimal colour code dictionary which displays a colour's hexadecimal colour code

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -2,7 +2,10 @@
 # create a dictionary containing the name of colour and its hexadecimal colour code
 # print user input (case-independent)
 
-COLOUR_TO_CODE = {"Amethyst": "#9966cc", "GhostWhite": "#f8f8ff", "GOGreen": "#00ab66", "Ginger": "#b06500", "AbsoluteZero": "#0048ba", "AcidGreen": "#b0bf1a", "BrightLilac": "#d891ef", "BrightUbe": "#d19fe8", "Bronze": "#cd7f32", "Celeste": "#b2ffff"}
+COLOUR_TO_CODE = {"Amethyst": "#9966cc", "GhostWhite": "#f8f8ff", "GOGreen": "#00ab66",
+                  "Ginger": "#b06500", "AbsoluteZero": "#0048ba", "AcidGreen": "#b0bf1a",
+                  "BrightLilac": "#d891ef", "BrightUbe": "#d19fe8", "Bronze": "#cd7f32",
+                  "Celeste": "#b2ffff"}
 print(COLOUR_TO_CODE)
 
 
