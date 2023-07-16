f91b0a65c2e05e4bcf5cc4b4736832d3f89720a4
HenevianAcero <henevian.acero@my.jcu.edu.au>
2023-07-16 22:57:23 +1000

    A program that uses a class to check the details of programming languages and returns dynamically typed languages

---
+++ prac_06/languages.py
@@ -0,0 +1,22 @@
+"""
+CP14014 Practical 6
+languages.py
+Estimated time: 15 mins
+Actual time: 21 mins
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+def main():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic",True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    languages = [ruby, python, visual_basic]
+    print("The dynamically typed languages:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+main()
