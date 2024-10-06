ceb26b43e1490875974d42f4562371ec066df5cd
HenevianAcero <henevian.acero@my.jcu.edu.au>
2024-10-06 18:45:32 +1000

    Print random number between 1-100

---
+++ prac_03/randoms.py
@@ -0,0 +1,19 @@
+"""
+CP1404 - Practical 3
+randoms.py
+
+"""
+
+# What did you see on line 1? 17
+# What was the smallest number you could have seen, what was the largest? smallest is5, largest is 20
+
+# What did you see on line 2? 3
+#What was the smallest number you could have seen, what was the largest? smallest is 3, largest is 10
+# Could line 2 have produced a 4? No
+
+# What did you see on line 3? 3.1305279480383836
+# What was the smallest number you could have seen, what was the largest? smallest is 2.5, largest is 5.5
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+import random
+print(random.randint(1, 100))
