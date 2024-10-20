d46428660d22aa5fee252e2bbc6006c8c87dd6a2
HenevianAcero <henevian.acero@my.jcu.edu.au>
2024-10-20 23:23:20 +1000

    Format output to include parenthesis

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -19,7 +19,7 @@ def main():
         email = input("Email: ")
     # email = email_to_user.get(user, 0)
     for email, user in email_to_user.items():
-        print(f"{user} {email}")
+        print(f"{user} ({email})")
 def get_user_from_email(email):
     prefix = email.split('@')[0]
     parts = prefix.split('.')
